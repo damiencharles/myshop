bba138b39f311a27fa702389ddfa6c70d4719eb2
Damien Charles <66057431+damiencharles@users.noreply.github.com>
2020-07-21 19:35:04 +0200

    Add files via upload

--- equipe.php
+++ equipe.php
@@ -2,13 +2,13 @@
 
 <div class="container my-5">
     <div class="row">
-        <div class="col-6 mt-5 ">
+        <div class="col-sm-12 col-md-6 order-sm-2 order-md-1 mt-5 ">
             <h2 class="mt-5">L'équipe de "La Maison du slip"</h2>
             <p class="mt-3">La maison du slip, c'est une équipe de 3 personnes, qui travaillent ensemble pour vous proposer des outils de création de site web.</p>
             <a href="index.php" class="btn btn-danger btn-lg active" role="button" aria-pressed="true">Découvrir le site</a>
         </div>
-        <div class="col-6">
-          <img src="./assets/img/support.png" width="100%" class="">
+        <div class="col-sm-12 col-md-6 order-sm-1 order-md-2 mt-5">
+          <img src="./assets/img/support.png" width="100%" class="img-fluid">
         </div>
     </div>
 </div>
@@ -19,33 +19,20 @@
             <h2 class="mt-4">Les membres de l'équipe</h2>
             <div class="mt-4 dropdown-divider"></div>
         </div>
-        
+    </div>   
         <div class="row text-center mt-5">
-            <div class="col-4">
-                <img src="./assets/img/Fichier3.png" width="50%" class="">
-                <h3 class="mt-5">Damien</h3>
-                <h4 class="mt-4">Lead Développeur</h4>
-                <p class="mt-3">Damien est le chef des développeurs.
-                    Il est en charge du code et de la stabilité de la plateforme.
-                    C'est aussi un défenseur de l'extrême au baby-foot (pas plus de 2 buts encaissés en moyenne par match).</p>
+        <?php 
+        require ('./src/DevManager.php');
+        $devs = DevManager::getDev();
+        foreach ($devs as $dev){?>
+            <div class="col-sm-12 col-md-4">
+                <img src="<?php echo $dev->photo_dev?>" width="50%" class="img-fluid">
+                <h3 class="mt-5"><?php echo $dev->nom_dev?></h3>
+                <h4 class="mt-4"><?php echo $dev->poste_dev?></h4>
+                <p class="mt-3"> <?php echo $dev->bio_dev?>.</p>
             </div>
-            <div class="col-4">
-                <img src="./assets/img/Fichier4.png" width="50%" class="">
-                <h3 class="mt-5">Louis</h3>
-                <h4 class="mt-4">Développeur</h4>
-                <p class="mt-3">Comme 100% des développeurs de l'équipe, Louis code.
-                    Il fait partie de ces gens qu'on ne voit pas ou qu'on entend peu mais qui contribuent à améliorer les sites web. (Adepte de la Fistinière...)</p>
-            </div>
-            <div class="col-4">
-                <img src="./assets/img/Fichier5.png" width="50%" class="">
-                <h3 class="mt-5">Frederic</h3>
-                <h4 class="mt-4">Développeur</h4>
-                <p class="mt-3">Frederic est en alternance a la " Maison du Slip" .
-                    Une semaine sur deux, il développe pour vous de nouvelles fonctionnalités.
-                    Il est bien souvent celui qui met en production de petits bugs. (Boit beaucoup de café et passe son temps à râler)</p>
-            </div>
+<?php } ?>
         </div>
-    </div>
 </div>
 
 <?php include ('footer.php')?>
